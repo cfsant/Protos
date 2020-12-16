b4440035d31000bcd7b14eebd8b30e12e5bf6161
CAIO SANTANA <cfs.plus.s8@gmail.com>
2020-12-16 10:36:32 -0300

    Update

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 !#/bin/bash
 
-PROTO_DIR=./proto
+PROTO_DIR=./Protos
 
 # Generate TypeScript code (d.ts)
 yarn run grpc_tools_node_protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --ts_out=${PROTO_DIR} \
-    -I ./proto \
-    proto/*.proto
+    -I ./Protos \
+    Protos/*.proto
